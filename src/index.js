a2ada0d2fe6f66078d8a05985a6c40032d454ff5
Don Stuckey <don.stuckey@ed.ac.uk>
2021-07-12 23:47:37 +0100

    App now displays latitude of users location or an error message.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    return <div>Hi there!</div>
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('A');
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        };
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => { 
+                console.log('B'); 
+                this.setState({ 
+                    lat: position.coords.latitude
+                 }); 
+                console.log('C'); 
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message })
+            }
+        );
+        console.log('D');
+    }
+
+    render() {
+        console.log('E');
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+        return <div>Loading...</div>;
+    }
+}
 
 ReactDOM.render(
     <App />, 
