6e2847f29d2df90f43d48367134827e357a7ae42
Don Stuckey <don.stuckey@ed.ac.uk>
2021-07-17 21:11:02 +0100

    Finished season project.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     constructor(props) {
@@ -18,15 +19,22 @@ class App extends React.Component {
         );
     }
 
-    render() {
-        
+    renderConent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
         if (!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />
         }
-        return <div>Loading...</div>;
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderConent()}
+            </div>
+        )
     }
 }
 
